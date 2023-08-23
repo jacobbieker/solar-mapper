d0b5fe789d35707d1f5316679f00e16fe1356c67
Jacob Bieker <jacob@bieker.tech>
2023-08-23 22:34:57 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
+from pathlib import Path
 
 from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+install_requires = (this_directory / "requirements.txt").read_text().splitlines()
+
 
 setup(
     name="solar_mapper",
@@ -9,6 +15,8 @@ setup(
     author="Jacob Bieker",
     author_email="jacob@bieker.tech",
     url="https://github.com/jacobbieker/solar-mapper",  # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    install_requires=["pytorch-lightning", "hydra-core"],
+    install_requires=install_requires,
+    long_description=long_description,
+    include_package_data=True,
     packages=find_packages(),
 )
