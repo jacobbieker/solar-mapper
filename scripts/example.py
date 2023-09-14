b356ed64aedfe0f872bde5532398c0d6a51e1f78
Jacob Bieker <jacob@bieker.tech>
2023-09-14 19:16:38 +0100

    Add plotting segmentation mask to example

--- scripts/example.py
+++ scripts/example.py
@@ -14,3 +14,15 @@ print(train_example)
 seg_mask = train_example['segmentation_map']
 # How many pixels are non-zero in the segmentation map? Should come out to 394
 print(f"Number of non-zero pixels in segmentation map: {seg_mask.sum().values}")
+import matplotlib.pyplot as plt
+import numpy as np
+# Get the index of the first non-zero element
+non_zero_idx = np.nonzero(seg_mask.values)
+print(non_zero_idx)
+# Get the center of the non-zero elements
+non_zero_center = np.mean(non_zero_idx, axis=1)
+print(non_zero_center)
+# Get the cutout
+seg_mask_cutout = seg_mask.where(seg_mask != 0).isel(x=slice(int(non_zero_center[1]-100), int(non_zero_center[1]+100)), y=slice(int(non_zero_center[0]-100), int(non_zero_center[0]+100)))
+plt.imshow(seg_mask_cutout)
+plt.show()
